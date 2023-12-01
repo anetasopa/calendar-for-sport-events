e7ccc42449a210c47b46d17da750bf2eeebbe09f
Aneta <sopaaneta@gmail.com>
2023-12-01 16:30:18 +0100

    Style add event page

--- src/AddEvent/AddEvent.js
+++ src/AddEvent/AddEvent.js
@@ -5,6 +5,12 @@ const AddEvent = ({ onAddEvent }) => {
   const [formData, setFormData] = useState({
     eventName: '',
     eventDate: '',
+    eventStatus: '',
+    eventHomeTeamName: '',
+    eventHomeTeamSlug: '',
+    eventAwayTeamName: '',
+    eventAwayTeamSlug: '',
+    eventResult: '',
   });
 
   const handleInputChange = (e) => {
@@ -39,6 +45,62 @@ const AddEvent = ({ onAddEvent }) => {
           id="eventDate"
           name="eventDate"
           value={formData.eventDate}
+          onChange={handleInputChange}
+          className="add-event-input"
+        />
+        <label htmlFor="eventStatus">Event Status:</label>
+        <input
+          type="text"
+          id="eventStatus"
+          name="eventStatus"
+          value={formData.eventStatus}
+          onChange={handleInputChange}
+          className="add-event-input"
+        />
+        <h4>Home Team:</h4>
+        <label htmlFor="eventHomeTeamName">Home Team Name:</label>
+        <input
+          type="text"
+          id="eventHomeTeamName"
+          name="eventHomeTeamName"
+          value={formData.eventHomeTeamName}
+          onChange={handleInputChange}
+          className="add-event-input"
+        />
+        <label htmlFor="eventHomeTeamSlug">Home Team Slug:</label>
+        <input
+          type="text"
+          id="eventHomeTeamSlug"
+          name="eventHomeTeamSlug"
+          value={formData.eventHomeTeamSlug}
+          onChange={handleInputChange}
+          className="add-event-input"
+        />
+        <h4>Away Team:</h4>
+        <label htmlFor="eventAwayTeamName">Away Team Name:</label>
+        <input
+          type="text"
+          id="eventAwayTeamName"
+          name="eventAwayTeamName"
+          value={formData.eventAwayTeamName}
+          onChange={handleInputChange}
+          className="add-event-input"
+        />
+        <label htmlFor="eventAwayTeamSlug">Away Team Slug:</label>
+        <input
+          type="text"
+          id="eventAwayTeamSlug"
+          name="eventAwayTeamSlug"
+          value={formData.eventAwayTeamSlug}
+          onChange={handleInputChange}
+          className="add-event-input"
+        />
+        <label htmlFor="eventResult">Result:</label>
+        <input
+          type="text"
+          id="eventResult"
+          name="eventResult"
+          value={formData.eventResult}
           onChange={handleInputChange}
           className="add-event-input"
         />
